26d2a242275ad6f582f78e39a1b4117bb9e9ed0c
natbelo <nataliabelo.nb@gmail.com>
2021-08-16 22:05:13 -0300

    Finalizado o script excluirPet (Delete)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -72,4 +72,40 @@ public class Pet {
         System.out.println("O token é " + token);
     }
 
+    @Test(priority = 3)
+    public void alterarPet() throws Exception {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name",is("Star"))
+                .body("status", is("sold"))
+        ;
+    }
+
+    @Test(priority = 4)
+    public void excluirPet(){
+        String petId = "201925062021";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code",is(200))
+                .body("type",is("unknown"))
+                .body("message", is(petId))
+        ;
+    }
+
 }
